fde11654e1363158ed22bd37244a60033e12a9cd
OLGA DOBROVOLSKA <3250186@gmail.com>
2022-09-29 16:18:57 +0300

    creating file

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+const wilder1 = "Olga";
+const wilder2 = "Ludovic";
+const wilder3 = "Vitalii";
+const wilder4 = "Olena";
+console.log(chalk.blue(wilder1));
+console.log(chalk.red(wilder2));
+console.log(chalk.white(wilder3));
+console.log(chalk.green(wilder4));
